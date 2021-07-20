da98ac5c68fa692a1e5e6ecc1972a92901bd8eb4
Martin Schwartzman <ugmartin@hotmail.com>
2021-07-20 19:32:39 -0300

    .

--- main.tf
+++ main.tf
@@ -1,22 +1,13 @@
 provider "aws" {
   version = "~> 2.7"
-  region  = "us-east-1"
+  region  = "us-east-2"
 }
 
 resource "aws_instance" "web" {  
-  ami = "ami-09e67e426f25ce0d7"
+  ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
-  
-  // tags = {
-  //   Name = "devops-one"
-  // }
 }
 
 output "public_ip" {
   value = "${aws_instance.web.public_ip}"
 }
-
-// output "private_ip" {
-//   value = "${aws_instance.web.private_ip}"
-// }
-ECHO est  activado.
